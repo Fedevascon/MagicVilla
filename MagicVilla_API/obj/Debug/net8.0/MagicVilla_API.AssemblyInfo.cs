b3534d92b648c2603f4cbd8d390797c54363e159
Fedevascon <fv3578@gmail.com>
2024-06-06 05:43:27 -0300

    agregar insertar en bd

--- MagicVilla_API/obj/Debug/net8.0/MagicVilla_API.AssemblyInfo.cs
+++ MagicVilla_API/obj/Debug/net8.0/MagicVilla_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459228a42199f6b72114fd6991ba6922c8d3ded8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10b5cb5d66691470cce11789e881b26cbb28f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
